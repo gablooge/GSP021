5df793b0e65bd8754b361f61c9687930706962bf
Samsul Hadi <samsulhadikediri@gmail.com>
2020-10-02 13:18:21 +0700

    first commit

---
+++ 2.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# while [[ -z "$(gcloud config get-value core/account)" ]]; 
+# do echo "waiting login" && sleep 2; 
+# done
+
+# while [[ -z "$(gcloud config get-value project)" ]]; 
+# do echo "waiting project" && sleep 2; 
+# done
+
+
+gcloud config set compute/zone us-central1-b
+
+
+cd orchestrate-with-kubernetes/kubernetes
+
+kubectl create deployment nginx --image=nginx:1.10.0
+kubectl expose deployment nginx --port 80 --type LoadBalancer
+
+
+kubectl create secret generic tls-certs --from-file tls/
+kubectl create configmap nginx-proxy-conf --from-file nginx/proxy.conf
+kubectl create -f pods/secure-monolith.yaml
+
+kubectl create -f services/monolith.yaml
+
+kubectl label pods secure-monolith 'secure=enabled'
+
+kubectl create -f services/frontend.yaml
+kubectl create -f deployments/auth.yaml
+kubectl create -f services/auth.yaml
+kubectl create -f deployments/hello.yaml
+kubectl create -f services/hello.yaml
+
+kubectl create -f deployments/frontend.yaml
+
+kubectl create configmap nginx-frontend-conf --from-file=nginx/frontend.conf
+
+gcloud compute firewall-rules create allow-monolith-nodeport \
+  --allow=tcp:31000
+
+
+
---
+++ README.md
@@ -0,0 +1,33 @@
+# GSP021
+
+Orchestrating the Cloud with Kubernetes
+
+https://www.qwiklabs.com/focuses/557?parent=catalog
+
+
+# using Cloudshell
+> git clone https://github.com/gablooge/GSP021.git
+
+> cd GSP021
+
+> sudo chmod +x cloudshell.sh
+
+> ./cloudshell.sh
+
+
+# using gcloud SDK Client
+## Terminal 1
+> git clone https://github.com/gablooge/GSP021.git
+
+> cd GSP021
+
+> sudo chmod +x 1.sh
+
+> ./1.sh
+
+
+## Terminal 2 for gcloud authentication
+> gcloud init
+
+
+
